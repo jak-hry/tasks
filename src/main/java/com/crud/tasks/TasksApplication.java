fb7de4509e34e02e8336b33c9189faa773d78f72
Jakub Hryckiewicz <jak-hry@wp.pl>
2023-03-07 19:59:50 +0100

    22.2 Added

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -10,7 +10,6 @@ public class TasksApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
-
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(TasksApplication.class);
