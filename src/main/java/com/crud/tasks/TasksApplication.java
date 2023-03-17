7f79027515620a001c55d4541ae7ba1ffbc9e15e
Jakub Hryckiewicz <jak-hry@wp.pl>
2023-03-17 19:40:40 +0100

    23.1 corrections

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,17 +1,20 @@
 package com.crud.tasks;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@OpenAPIDefinition
 @SpringBootApplication
-public class TasksApplication extends SpringBootServletInitializer {
+//public class TasksApplication extends SpringBootServletInitializer {
+public class TasksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TasksApplication.class);
-	}
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(TasksApplication.class);
+//	}
 }
