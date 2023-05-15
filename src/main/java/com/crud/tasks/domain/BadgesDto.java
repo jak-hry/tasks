c9e44ee39986540a5ad0fac0f65f88d005bf277c
Jakub Hryckiewicz <jak-hry@wp.pl>
2023-05-15 18:14:43 +0200

    fdsafasd

--- src/main/java/com/crud/tasks/domain/BadgesDto.java
+++ src/main/java/com/crud/tasks/domain/BadgesDto.java
@@ -3,9 +3,11 @@ package com.crud.tasks.domain;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class BadgesDto {
 
     @JsonProperty("votes")
