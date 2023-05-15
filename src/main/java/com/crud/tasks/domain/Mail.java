3de6ddfb8ca7c8c4d5d02ea4dc694f7508d61518
Jakub Hryckiewicz <jak-hry@wp.pl>
2023-05-16 01:28:57 +0200

    Implement @NoArgsConstructor in multiple classes

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -1,18 +1,18 @@
 package com.crud.tasks.domain;
 
-import lombok.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Mail {
 
-    private final String mailTo;
-    private final String subject;
-    private final String message;
-    private final String[] toCc;
+    private String mailTo;
+    private String subject;
+    private String message;
+    private String[] toCc;
 }
